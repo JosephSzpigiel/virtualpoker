0fde2e9531c7d68a08d769e0fe2fe2a599a8632d
Joseph Szpigiel <josephszpigiel@gmail.com>
2023-09-22 15:03:32 -0400

    Changed selection color to just change opacity

--- index.js
+++ index.js
@@ -3,43 +3,20 @@ const currentTokens = document.querySelector('#tokens span')
 let currentBet = 1
 const betForm = document.querySelector('#bet-form')
 const error = document.querySelector('#error')
-
 const cardsDivs = document.querySelectorAll('.card')
 const draw = document.querySelector('#draw')
 const redraw = document.querySelector('#redraw')
-
-function unshade(e){
-    e.target.style.opacity= '100%'
-}
-function shade(e){
-    e.target.style.opacity= '50%'
-}
-
-function selectByClick(e){
-    if(e.target.style.opacity== 0.5){
-        e.target.style.opacity= '40%'
-        e.target.removeEventListener('mouseover', shade)
-        e.target.removeEventListener('mouseleave', unshade)
-        const discardIndicator = document.createElement('p')
-        discardIndicator.className = "discard"
-        discardIndicator.textContent = 'DISCARD'
-        e.target.parentNode.append(discardIndicator)
-    }else{
-        e.target.style.opacity = '50%'
-        e.target.addEventListener('mouseover', shade)
-        e.target.addEventListener('mouseleave', unshade)
-        e.target.parentNode.querySelector('.discard').remove()
-    }
-}
 
 for (card of cardsDivs){
     const cardImage = document.createElement('img')
     cardImage.src = 'https://deckofcardsapi.com/static/img/back.png'
     cardImage.classList.add('card-image')
     card.append(cardImage)
-}
+}   
 
-betForm.addEventListener('submit',(e)=>{
+betForm.addEventListener('submit', gameStart)
+
+function gameStart(e){
     e.preventDefault()
     try{
         document.querySelector('#result').remove()
@@ -58,28 +35,30 @@ betForm.addEventListener('submit',(e)=>{
             if (newTokens < 0){
                 error.textContent = 'Not Enough Tokens!'
             }else{
-            error.textContent = ''
-            currentTokens.textContent = newTokens
-            redraw.disabled = false
-            betForm.submit.disabled = true
-            betForm.bet.disabled = true
-            data.cards.forEach((card,index) => {
-                const cardImageInHand = document.querySelector('#card'+index+ ' img')
-                cardImageInHand.src = card.image
-                cardImageInHand.id = card.image.split('/')[5].split('.')[0]
-                cardImageInHand.style.opacity='100%'
-                cardImageInHand.addEventListener('mouseover', shade)
-                cardImageInHand.addEventListener('mouseleave', unshade)
-                cardImageInHand.addEventListener('click', selectByClick)
+                error.textContent = ''
+                currentTokens.textContent = newTokens
+                redraw.disabled = false
+                betForm.submit.disabled = true
+                betForm.bet.disabled = true
+                data.cards.forEach((card,index) => {
+                    const cardImageInHand = document.querySelector('#card'+index+ ' img')
+                    cardImageInHand.src = card.image
+                    cardImageInHand.id = card.image.split('/')[5].split('.')[0]
+                    cardImageInHand.style.opacity='100%'
+                    cardImageInHand.addEventListener('mouseover', shade)
+                    cardImageInHand.addEventListener('mouseleave', unshade)
+                    cardImageInHand.addEventListener('click', selectByClick)
             })}
             }        
         )})
-})
+}
 
 // create button that will find selected cards and will discard them,
 // and thne draw the same number of cards and replace them
 
-redraw.addEventListener('click', (e) =>{
+redraw.addEventListener('click', endGame)
+
+function endGame(e){
     const cardImgs = document.querySelectorAll('.card-image')
     const imgToReplace = []
     const cardsToDiscard = []
@@ -209,7 +188,7 @@ redraw.addEventListener('click', (e) =>{
             })
         })
     
-})
+}
 
 function findPairs(cardsArray){
     let hasPairs = false
@@ -312,4 +291,29 @@ function findStraight(cardsArray){
         }
     }
     return [isStraight, isRoyal]
+}
+
+function unshade(e){
+    e.target.style.opacity= '100%'
+}
+
+function shade(e){
+    e.target.style.opacity= '50%'
+}
+
+function selectByClick(e){
+    if(e.target.style.opacity== 0.5){
+        e.target.style.opacity= '40%'
+        e.target.removeEventListener('mouseover', shade)
+        e.target.removeEventListener('mouseleave', unshade)
+        const discardIndicator = document.createElement('p')
+        discardIndicator.className = "discard"
+        discardIndicator.textContent = 'DISCARD'
+        e.target.parentNode.append(discardIndicator)
+    }else{
+        e.target.style.opacity = '50%'
+        e.target.addEventListener('mouseover', shade)
+        e.target.addEventListener('mouseleave', unshade)
+        e.target.parentNode.querySelector('.discard').remove()
+    }
 }
